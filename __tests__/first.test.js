5f7ced4c5b808eb7d519b880f5a5d801d3ed892c
Stepan Shi <ste-shi@mail.ru>
2021-05-31 18:02:04 +0300

    tests delete

--- __tests__/first.test.js
+++
@@ -1,9 +0,0 @@
-import { test, expect, describe } from '@jest/globals';
-import sum from '../bin/firstbin.js';
-
-describe('test description', () => {
-  test('test Sum', () => {
-    const actualResult = sum(3, 5);
-    expect(actualResult).toEqual(8);
-  });
-});
