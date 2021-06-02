49beda17723f61a235dda6e75bcc1a3330b8a5aa
Stepan Shi <ste-shi@mail.ru>
2021-06-02 19:31:48 +0300

    setting up

--- __tests__/first.test.js
+++ __tests__/first.test.js
@@ -1,9 +1,8 @@
 import { test, expect, describe } from '@jest/globals';
-import sum from '../bin/firstbin.js';
 
 describe('test description', () => {
   test('test Sum', () => {
-    const actualResult = sum(3, 5);
+    const actualResult = 3 + 5;
     expect(actualResult).toEqual(8);
   });
 });
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 function component() {
   const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = 'LEEFFFFEEE';
-
+  element.innerHTML = 'LEEE';
   return element;
 }
 
