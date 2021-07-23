74ea57052202b88519e9b98e08903004593fbdc4
Stepan Shi <ste-shi@mail.ru>
2021-07-23 15:39:17 +0300

    test del

--- Makefile
+++ Makefile
@@ -4,7 +4,3 @@ build:
 	npm run build
 lint:
 	npx eslint .
-test:
-	npm test
-test-coverage:
-	npm test -- --coverage --coverageProvider=v8
--- __tests__/first.test.js
+++
@@ -1,8 +0,0 @@
-import { test, expect, describe } from '@jest/globals';
-
-describe('test description', () => {
-  test('Sum', () => {
-    const actualResult = 3 + 5;
-    expect(actualResult).toEqual(8);
-  });
-});
