4f056874257220eff819d6cfc8385dcf99ecbc75
Stepan Shi <ste-shi@mail.ru>
2021-07-28 20:29:19 +0300

    passing test

--- src/handlers.js
+++ src/handlers.js
@@ -45,6 +45,7 @@ const makeResponse = (state, link) => {
       state.rssForm.alreadyAddedRsss.push(link);
       state.rssForm.feedback = 'success';
       state.rssForm.state = 'successfully responsed';
+      console.log(222222222222, document.body.innerHTML);
     })
     .catch(() => {
       state.rssForm.feedback = 'errors.noValidRss';
