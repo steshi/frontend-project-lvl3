332f9827bcee3fe1993eb7509663b2a9381c2ba9
Stepan Shi <ste-shi@mail.ru>
2021-07-29 17:28:09 +0300

    p

--- src/view.js
+++ src/view.js
@@ -34,6 +34,7 @@ const viewedOrNotClass = (state, id) => {
 };
 
 const renderData = (state, i18nInstance) => {
+  console.log('----------', 'renderDATA', document.body.innerHTML);
   if (state.data.feeds.length > 0) {
     const posts = document.querySelector('.posts');
     posts.innerHTML = `<div class="card-body posts-container"><h2 class="card-title h4">${i18nInstance.t('posts')}</h2></div><ul class="list-group border-0 rounded-0 postsList"></ul>`;
@@ -69,6 +70,7 @@ const renderData = (state, i18nInstance) => {
 };
 
 const renderFormSuccess = (state, i18nInstance, elements) => {
+  console.log('----------', 'renderFORMSucess', document.body.innerHTML);
   elements.addButton.disabled = false;
   elements.input.classList.remove('is-invalid');
   elements.feedback.classList.remove('text-danger');
@@ -79,6 +81,7 @@ const renderFormSuccess = (state, i18nInstance, elements) => {
 };
 
 const renderFormFail = (state, i18nInstance, elements) => {
+  console.log('----------', 'renderFormFail', document.body.innerHTML);
   elements.addButton.disabled = false;
   if (elements.feedback.classList.contains('text-success')) {
     elements.feedback.classList.replace('text-success', 'text-danger');
@@ -88,10 +91,13 @@ const renderFormFail = (state, i18nInstance, elements) => {
 };
 
 const renderFormPending = (elements) => {
+  console.log('----------', 'renderPending', document.body.innerHTML);
   elements.addButton.disabled = true;
 };
 
 const render = (state, i18nInstance) => {
+  console.log('----------', 'render', document.body.innerHTML);
+
   const elements = {
     feedback: document.querySelector('.feedback'),
     input: document.querySelector('#url-input'),
@@ -120,8 +126,8 @@ const render = (state, i18nInstance) => {
 };
 
 const visualize = (state, i18nInstance) => {
-  const watchedState = onChange(state, (path, value) => {
-    console.log('RENDERING STATE', '\n', 'PATH:', path, '\n', 'VALUE', value);
+  const watchedState = onChange(state, () => {
+    // console.log('RENDERING STATE', '\n', 'PATH:', path, '\n', 'VALUE', value);
     render(state, i18nInstance);
   });
   return watchedState;
