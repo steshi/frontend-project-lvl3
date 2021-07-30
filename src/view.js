815062f91f25dc34d48e20368258777427c49773
Stepan Shi <ste-shi@mail.ru>
2021-07-30 11:50:31 +0300

    refactoring

--- src/view.js
+++ src/view.js
@@ -1,20 +1,22 @@
 /* eslint-disable no-param-reassign */
 import onChange from 'on-change';
 
-const renderModal = (state, event, i18nInstance) => {
-  const postLi = event.target.parentNode;
-  const liId = Number.parseInt(postLi.id, 10);
+const renderViewedPosts = (state, e) => {
+  const liId = Number.parseInt(e.target.parentElement.id, 10);
   state.stateUi.forEach((linkState) => {
-    if (linkState.postId === liId) {
-      linkState.viewed = true;
-    }
+    if (linkState.postId === liId) linkState.viewed = true;
   });
+};
+
+const renderModal = (state, event, i18nInstance) => {
+  renderViewedPosts(state, event);
+  const liId = Number.parseInt(event.target.parentElement.id, 10);
   const modal = document.querySelector('#detailModal');
   const post = state.data.posts.filter((el) => el.id === liId)[0];
   modal.innerHTML = `<div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="detailModalLabel">${post.title}</h5>
+          <h5 class="modal-title" id="detailModal">${post.title}</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
@@ -29,48 +31,50 @@ const renderModal = (state, event, i18nInstance) => {
 };
 
 const viewedOrNotClass = (state, id) => {
-  const filtered = state.stateUi.filter((element) => element.postId === id);
-  return filtered[0].viewed ? 'fw-normal' : 'fw-bold';
+  const post = state.stateUi.filter((element) => element.postId === id)[0];
+  return post.viewed ? 'fw-normal' : 'fw-bold';
+};
+
+const renderPosts = (state, i18nInstance) => {
+  const posts = document.querySelector('.posts');
+  posts.innerHTML = `<div class="card-body posts-container"><h2 class="card-title h4">${i18nInstance.t('posts')}</h2></div><ul class="list-group border-0 rounded-0 postsList"></ul>`;
+  const postsHTML = state.data.posts
+    .map((post) => `<li id="${post.id}" class="list-group-item d-flex justify-content-between align-items-start border-0 border-end-0">
+      <a href="${post.link}" class="${viewedOrNotClass(state, post.id)}" target="_blank" rel="noopener noreferrer">${post.title}</a>
+      <button type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#detailModal">
+      ${i18nInstance.t('show')}
+    </button></li>`)
+    .join('');
+  posts.querySelector('.postsList').innerHTML = postsHTML;
+  const buttons = posts.querySelectorAll('button');
+  buttons.forEach((button) => button.addEventListener('click', (event) => {
+    renderModal(state, event, i18nInstance);
+  }));
+  const links = posts.querySelectorAll('a');
+  links.forEach((link) => link.addEventListener('click', (event) => {
+    renderViewedPosts(state, event);
+  }));
+};
+
+const renderFeeds = (state, i18nInstance) => {
+  const feeds = document.querySelector('.feeds');
+  feeds.innerHTML = `<div class="card-body feeds-contatiner"><h2 class="card-title h4">${i18nInstance.t('feeds')}</h2></div><ul class="list-group border-0 rounded-0 feedsList"></ul>`;
+  const feedsHTML = state.data.feeds
+    .map((feed) => `<li><h3 class="h6 m-0">${feed.title}</h3><p class="m-0 small text-black-50">${feed.description}</p></li>`)
+    .join('');
+  feeds.querySelector('.feedsList').innerHTML = feedsHTML;
 };
 
 const renderData = (state, i18nInstance) => {
-  // console.log('----------', 'renderDATA');
+  console.log('----------', 'renderDATA');
   if (state.data.feeds.length > 0) {
-    const posts = document.querySelector('.posts');
-    posts.innerHTML = `<div class="card-body posts-container"><h2 class="card-title h4">${i18nInstance.t('posts')}</h2></div><ul class="list-group border-0 rounded-0 postsList"></ul>`;
-    const postsHTML = state.data.posts
-      .map((post) => `<li id="${post.id}" class="list-group-item d-flex justify-content-between align-items-start border-0 border-end-0">
-        <a href="${post.link}" class="${viewedOrNotClass(state, post.id)}" target="_blank" rel="noopener noreferrer">${post.title}</a>
-        <button type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#detailModal">
-        ${i18nInstance.t('show')}
-      </button></li>`)
-      .join('');
-    posts.querySelector('.postsList').innerHTML = postsHTML;
-    const buttons = posts.querySelectorAll('button');
-    buttons.forEach((button) => button.addEventListener('click', (event) => {
-      renderModal(state, event, i18nInstance);
-    }));
-    const links = posts.querySelectorAll('a');
-    links.forEach((link) => link.addEventListener('click', (event) => {
-      const liId = parseInt(event.target.parentElement.id, 10);
-      state.stateUi.forEach((linkState) => {
-        if (linkState.postId === liId) {
-          linkState.viewed = true;
-        }
-      });
-    }));
-
-    const feeds = document.querySelector('.feeds');
-    feeds.innerHTML = `<div class="card-body feeds-contatiner"><h2 class="card-title h4">${i18nInstance.t('feeds')}</h2></div><ul class="list-group border-0 rounded-0 feedsList"></ul>`;
-    const feedsHTML = state.data.feeds
-      .map((feed) => `<li><h3 class="h6 m-0">${feed.title}</h3><p class="m-0 small text-black-50">${feed.description}</p></li>`)
-      .join('');
-    feeds.querySelector('.feedsList').innerHTML = feedsHTML;
+    renderPosts(state, i18nInstance);
+    renderFeeds(state, i18nInstance);
   }
 };
 
 const renderFormSuccess = (state, i18nInstance, elements) => {
-  // console.log('----------', 'renderFORMSucess');
+  console.log('----------', 'renderFORMSucess');
   elements.addButton.disabled = false;
   elements.input.readOnly = false;
   elements.input.classList.remove('is-invalid');
@@ -82,7 +86,7 @@ const renderFormSuccess = (state, i18nInstance, elements) => {
 };
 
 const renderFormFail = (state, i18nInstance, elements) => {
-  // console.log('----------', 'renderFormFail');
+  console.log('----------', 'renderFormFail');
   elements.addButton.disabled = false;
   elements.input.readOnly = false;
   if (elements.feedback.classList.contains('text-success')) {
@@ -93,13 +97,13 @@ const renderFormFail = (state, i18nInstance, elements) => {
 };
 
 const renderFormPending = (elements) => {
-  // console.log('----------', 'renderPending');
+  console.log('----------', 'renderPending');
   elements.addButton.disabled = true;
   elements.input.readOnly = true;
 };
 
 const render = (state, i18nInstance) => {
-  // console.log('----------', 'render');
+  console.log('----------', 'render');
 
   const elements = {
     feedback: document.querySelector('.feedback'),
@@ -129,8 +133,8 @@ const render = (state, i18nInstance) => {
 };
 
 const visualize = (state, i18nInstance) => {
-  const watchedState = onChange(state, () => {
-  // console.log('RENDERING STATE', '\n', 'PATH:', path, '\n', 'VALUE', value);
+  const watchedState = onChange(state, (path, value) => {
+    console.log('RENDERING STATE', '\n', 'PATH:', path, '\n', 'VALUE', value);
     render(state, i18nInstance);
   });
   return watchedState;
